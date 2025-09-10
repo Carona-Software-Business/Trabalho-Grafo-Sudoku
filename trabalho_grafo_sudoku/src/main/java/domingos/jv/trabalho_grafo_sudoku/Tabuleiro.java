852d34774acd3e8e2d2ad4713abae6baa77011bf
Kauan Gomes Cardoso <kauangomescardoso100@gmail.com>
2025-09-10 11:45:32 -0300

    Classe Tabuleiro

--- trabalho_grafo_sudoku/src/main/java/domingos/jv/trabalho_grafo_sudoku/Tabuleiro.java
+++ trabalho_grafo_sudoku/src/main/java/domingos/jv/trabalho_grafo_sudoku/Tabuleiro.java
@@ -1,5 +1,37 @@
 package domingos.jv.trabalho_grafo_sudoku;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Tabuleiro {
-    
+
+    private String caminho = "inst1.txt";
+
+    private int[][] tabuleiro = new int[9][9];
+
+    public Tabuleiro() {
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String linha;
+            int i = 0;
+
+            while ((linha = br.readLine()) != null && i < 9) {
+                String[] partes = linha.trim().split(",");
+
+                for (int j = 0; j < 9 && j < partes.length; j++) {
+                    tabuleiro[i][j] = Integer.parseInt(partes[j].trim());
+                }
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(tabuleiro[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- trabalho_grafo_sudoku/src/main/java/domingos/jv/trabalho_grafo_sudoku/Trabalho_grafo_sudoku.java
+++ trabalho_grafo_sudoku/src/main/java/domingos/jv/trabalho_grafo_sudoku/Trabalho_grafo_sudoku.java
@@ -3,6 +3,7 @@ package domingos.jv.trabalho_grafo_sudoku;
 public class Trabalho_grafo_sudoku {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Tabuleiro tabuleiro = new Tabuleiro();
+        
     }
 }
