3e3957dfdb2d074a254f35c383fa9593c677ff9e
Kauan Gomes Cardoso <kauangomescardoso100@gmail.com>
2025-10-06 14:39:41 -0300

    Final

--- trabalho_grafo_sudoku/src/main/java/domingos/jv/trabalho_grafo_sudoku/Trabalho_grafo_sudoku.java
+++ trabalho_grafo_sudoku/src/main/java/domingos/jv/trabalho_grafo_sudoku/Trabalho_grafo_sudoku.java
@@ -5,16 +5,18 @@ import java.awt.Toolkit;
 public class Trabalho_grafo_sudoku {
 
     public static void main(String[] args) {
-        Tabuleiro tabuleiro = new Tabuleiro("inst3.txt");
+        Tabuleiro tabuleiro = new Tabuleiro("dificil.txt");
         
-        //System.out.println(tabuleiro.getGrafo());
+        Grafo resultadoL = tabuleiro.buscaLargura(100000000L);
+        Grafo resultadoP = tabuleiro.buscaProfundidade(100000000L);
         
-        //Grafo resultado = tabuleiro.buscaLargura(100000000L);
-        Grafo resultado = tabuleiro.buscaProfundidade(100000000L);
+        System.out.println("Resposta Largura: ");
+        if(resultadoL == null) System.out.println("Nao deu :(");
+        else System.out.println(resultadoL.printarTabuleiro());
         
-        System.out.println("Resposta: ");
-        if(resultado == null) System.out.println("Nao deu :(");
-        else System.out.println(resultado.printarTabuleiro());
+        System.out.println("Resposta Profundo: ");
+        if(resultadoP == null) System.out.println("Nao deu :(");
+        else System.out.println(resultadoP.printarTabuleiro());
         
         Toolkit.getDefaultToolkit().beep();
     }
